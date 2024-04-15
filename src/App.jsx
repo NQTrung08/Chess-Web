b431fe415430df474bff8428a88f045b87211b35
NQTrung08 <nqtrung0810@gmail.com>
2024-04-15 23:14:37 +0700

    feat: config .env.dev & .env.production

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 const App = () => {
   const [count, setCount] = useState(0)
    
@@ -16,7 +17,7 @@ const App = () => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React {`${import.meta.env.VITE_REACT_APP_NAME}`}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
