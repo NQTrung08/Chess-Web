e9c5a358d0ba4241011abe86c5b4dada53f8d87c
Huyen412 <ngochuyengiadinh4@gmail.orgg>
2024-04-19 23:37:04 +0700

    feat:code frontend login and register

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
-
+import { ConfigProvider } from 'antd';
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-   
-   
-
-       
-    
-  
-
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: 'main-color',
+        fontSize:16
+      },
+    }}
+  >
 
     <React.StrictMode>
       <BrowserRouter>
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
 
     </React.StrictMode>
-
+  </ConfigProvider>
 )
